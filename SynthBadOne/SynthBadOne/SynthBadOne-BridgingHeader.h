513fdd3e8d98eda922faa1e6fda4e85a9d3fd729
reversedFragment <39036349+reversedFragment@users.noreply.github.com>
2018-07-23 10:56:29 -0600

    Uploaded main project

---
+++ SynthBadOne/SynthBadOne/SynthBadOne-BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  SynthBadOne-BridgingHeader.h
+//  SynthBadOne
+//
+//  Created by Ben Adams on 7/23/18.
+//  Copyright © 2018 Ben Adams. All rights reserved.
+//
+
+#ifndef SenderSynth_BridgingHeader_h
+#define SenderSynth_BridgingHeader_h
+
+#import "Audiobus.h"
+
+#endif /* SenderSynth_BridgingHeader_h */
